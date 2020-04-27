c3c7cf7f4fdcbbf245a6daf326db96edf8ea372b
朱飞飞 <zhuffei1999@foxmail.com>
2020-04-28 02:31:27 +0800

    4.27 睡前

--- src/main/java/zhuffei/ffei/FfeiApplication.java
+++ src/main/java/zhuffei/ffei/FfeiApplication.java
@@ -3,13 +3,20 @@ package zhuffei.ffei;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("zhuffei.ffei.mapper")
-public class FfeiApplication {
+public class FfeiApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FfeiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FfeiApplication.class, args);
+    }
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
 }
